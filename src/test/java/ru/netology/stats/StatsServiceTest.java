00d4822e9ea65cd70df940b6fff80c940343aadd
Konstantin <kostenn07@mail.ru>
2023-04-05 13:27:26 +0300

    Reformat code

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,4 +1,5 @@
 package ru.netology.stats;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ public class StatsServiceTest {
 
     @Test
     public void shouldFindAverage() {
-        StatsService  service = new StatsService();
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedAverage = 15;
@@ -25,7 +26,7 @@ public class StatsServiceTest {
 
     @Test
     public void shouldFindMaxSales() {
-        StatsService  service = new StatsService();
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMaxSales = 8;
@@ -35,7 +36,7 @@ public class StatsServiceTest {
 
     @Test
     public void shouldFindMinSales() {
-        StatsService  service = new StatsService();
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMinSales = 9;
@@ -45,7 +46,7 @@ public class StatsServiceTest {
 
     @Test
     public void numberMonthsSalesBelowAverage() {
-        StatsService  service = new StatsService();
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMinMonthSales = 5;
@@ -55,7 +56,7 @@ public class StatsServiceTest {
 
     @Test
     public void numberMonthsSalesAboveAverage() {
-        StatsService  service = new StatsService();
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expectedMaxMonthSales = 5;
